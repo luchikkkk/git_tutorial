b51b5235a7cec3f08df222de2a25b1508655c4b5
PepsiColaS <workmail912@mail.ru>
2022-06-28 22:30:02 +0300

    Wrote remove_file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+import os
+
+def delete_flie():
+	file_path = 'file.txt'
+	try:
+		os.remove(file.path)
+		return True
+	except:
+		print("The system cannot find the file specified")
+		return False
