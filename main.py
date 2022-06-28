f087e00b54a69694ac8ef9b4a6e80e61f7dee3ba
luchikkkk <varvarskiy1@gmail.com>
2022-06-28 22:48:01 +0300

    Add main function

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ def write_in_txt_file():
 	with open('test.txt', 'w') as writer:
 		writer.write('I love Git')
 		return True
+
+
+if __name__ == "__main__":
+	if write_in_txt_file(): # создаем и пишем в файл "I love Git"
+		print("[OK] write")
+
+	print(read_txt_file()) #читаем содержимое
+
+	if delete_file(): #удаляем файл
+		print("[OK] delete")
